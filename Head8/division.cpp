9bd353ff04a91788b49979a8f3684eaa54516d51
Maxim <maixm79135@gmail.com>
2018-09-18 23:00:16 +0300

    6

---
+++ Head8/division.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int m, n;
+	cin >> m >> n;
+	cout << m / n;
+	m %= n;
+	if (m > 0) {
+		cout << ',';
+		int p = m;
+		int q = n;
+		while (p != q) q > p ? q -= p : p -= q;
+
+		m /= p;
+		n /= p;
+		p = 0; q = 0; int z = n;
+		while (z % 2 == 0) {
+			p++;
+			z /= 2;
+		}
+		while (z % 5 == 0) {
+			q++;
+			z /= 5;
+		}
+		if (p < q) p = q;
+		for (int i = 1; i <= p; i++) {
+			m *= 10;
+			cout << m / n;
+			m %= n;
+		}
+
+		if (z > 1) {
+			cout << '(';
+			q = 9; p = 1;
+			while (q % z > 0) {
+				p++;
+				q = q % z * 10 + 9;
+			}
+			for (int k = 1; k <= p; k++){
+				m *= 10;
+				cout << m / n;
+				m %= n;
+			}
+			cout << ')';
+		}
+	}
+	return 0;
+}
